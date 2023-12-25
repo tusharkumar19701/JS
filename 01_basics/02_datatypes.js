1910a3440e6d3b181c0833c431c5817d638374b2
Tushar Kumar <tusharkumar19701@gmail.com>
2023-12-25 04:20:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3); //we are using nodejs, not browser
+
+console.log(3+3); 
+console.log("Tushar");
+
+let name = "Tushar";
+let age = 20;
+let isLoggedIn = true;
+let state;
+
+//number
+//bigint
+//string 
+//boolean => true/false
+//null => standalone value
+//undefined => declared the variable but not defined the value
+//symbol => unique
+
+
+//object 
+
+console.log(typeof "Tushar");
+console.log(typeof null);   // object 
+console.log(typeof undefined);  // undefined
